045f1a670476d6436d633014766654090b87afcd
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-04-26 20:34:49 +0200

    Created new models EventTemp and Exception

---
+++ models/eventTemp.js
@@ -0,0 +1,63 @@
+'use strict';
+module.exports = function (sequelize, DataTypes) {
+    var EventTemp = sequelize.define('eventTemp', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        date: {
+            type: DataTypes.DATE
+        },
+        day: {
+            type: DataTypes.STRING
+        },
+        from: {
+            type: DataTypes.STRING(5)
+        },
+        to: {
+            type: DataTypes.STRING(5)
+        },
+        activity: {
+            type: DataTypes.STRING
+        },
+        type: {
+            type: DataTypes.STRING
+        },
+        type: {
+            type: DataTypes.STRING
+        },
+        tutorId: {
+            type: DataTypes.INTEGER
+        },
+
+        placeId: {
+            type: DataTypes.INTEGER
+        },
+        groupId: {
+            type: DataTypes.INTEGER
+        },
+
+        note: {
+            type: DataTypes.STRING
+        },
+        blocks: {
+            type: DataTypes.INTEGER
+        }
+    }, {
+        indexes: [{
+            name: 'unique',
+            unique: true,
+            fields: ['date',
+                'from',
+                'to',
+                'type',
+                'tutorId',
+                'placeId',
+                'groupId',
+                'blocks']
+        }],
+        timestamps: true
+    });
+    return EventTemp;
+};
---
+++ models/exception.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = function (sequelize, DataTypes) {
+    var Exception = sequelize.define('exception', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        key: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.ENUM,
+            values: [
+                //Group
+                'G',
+                //Room
+                'S',
+                //Tutor
+                'N'
+            ]
+        }
+    }, {
+        timestamps: true,
+        indexes: [
+            {
+                unique: true,
+                fields: ['key', 'type']
+            }
+        ]
+    });
+    return Exception;
+};
