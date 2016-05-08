755567c154ba4cf9b17ab8ce193386df3259a45d
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-05-08 15:54:12 +0200

    Fixed label model

--- models/label.js
+++ models/label.js
@@ -1,93 +1,96 @@
-'use strict';
-module.exports = (sequelize, DataTypes)=> {
-    var Event = sequelize.define('event', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        date: {
-            type: DataTypes.DATEONLY
-        },
-        day: {
-            type: DataTypes.INTEGER(1)
-        },
-        from: {
-            type: DataTypes.STRING(5)
-        },
-        to: {
-            type: DataTypes.STRING(5)
-        },
-        activity: {
-            type: DataTypes.STRING,
-            defaultValue: ''
-        },
-        type: {
-            type: DataTypes.STRING,
-            defaultValue: ''
-        },
-        tutorId: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        placeId: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        groupId: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        note: {
-            type: DataTypes.STRING,
-            defaultValue: ''
-        },
-        blocks: {
-            type: DataTypes.INTEGER
-        },
-        deleted: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
-    }, {
-        timestamps: false,
-        indexes: [{
-            name: 'unique_event',
+var Label = sequelize.define('label', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    timetableId: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    key: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    value: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    type: {
+        type: DataTypes.ENUM,
+        values: [
+            //Group
+            'G',
+            //Building
+            'B',
+            //Room
+            'S',
+            //Tutor
+            'N',
+            //Field
+            'F',
+            //Surname
+            'C',
+            //Type
+            'T',
+            //activity
+            'A',
+            // note
+            'I',
+            //Unknown to validate
+            '?'
+        ],
+        defaultValue: '?'
+    },
+    moodleId: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    parentText: {
+        type: DataTypes.STRING,
+        allowNull: true
+
+    },
+    parentId: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    orginal: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+}, {
+    timestamps: false,
+    indexes: [
+        {
             unique: true,
-            fields: ['date',
-                'from',
-                'to',
-                'activity',
-                'type',
-                'tutorId',
-                'placeId',
-                'groupId',
-                'blocks']
-        }],
-        classMethods: {
-            associate: (models) => {
-                Label.hasOne(Label, {
-                    as: 'parent',
-                    foreginKey: 'parentId',
-                    targetKey: 'id'
-                });
-                Label.hasMany(models.event, {
-                    as: 'tutors',
-                    foreignKey: 'id',
-                    targetKey: 'tutorId'
-                });
-                Label.hasMany(models.event, {
-                    as: 'places',
-                    foreignKey: 'id',
-                    targetKey: 'placeId'
-                });
-                Label.hasMany(models.event, {
-                    as: 'groups',
-                    foreignKey: 'id',
-                    targetKey: 'groupId'
-                });
-            }
+            fields: ['timetableId', 'key', 'type']
         }
-    });
-    return Event;
+    ],
+    classMethods: {
+        associate: (models) => {
+
+            Label.hasOne(Label, {
+                as: 'parent',
+                foreginKey: 'parentId',
+                targetKey: 'id'
+            });
+            Label.hasMany(models.event, {
+                as: 'tutors',
+                foreignKey: 'id',
+                targetKey: 'tutorId'
+            });
+            Label.hasMany(models.event, {
+                as: 'places',
+                foreignKey: 'id',
+                targetKey: 'placeId'
+            });
+            Label.hasMany(models.event, {
+                as: 'groups',
+                foreignKey: 'id',
+                targetKey: 'groupId'
+            });
+        }
+    }
+});
 };
