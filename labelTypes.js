c71429dcde81a37feed473f1bb727cc697e9afa5
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-05-13 10:43:26 +0200

    Fix - missing commas

--- labelTypes.js
+++ labelTypes.js
@@ -1,13 +1,13 @@
 const LABEL_TYPES = {
     ACTIVITY: 0,
-    BUILDING: 1
-    SURNAME: 2
-    FIELD: 3
-    GROUP: 4
-    NOTE: 5
-    TUTOR: 6
-    ROOM: 7
+    BUILDING: 1,
+    SURNAME: 2,
+    FIELD: 3,
+    GROUP: 4,
+    NOTE: 5,
+    TUTOR: 6,
+    ROOM: 7,
     TYPE: 8,
-    UNKNOWN:9
+    UNKNOWN: 9
 };
 module.exports = LABEL_TYPES;
